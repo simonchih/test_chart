16db418640b7d154e95e66448ac46a23cef435fd
Simon <ficstudio@yahoo.com.tw>
2020-10-18 11:05:49 +0800

    v0.0.1

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            double[] data1 = new double[9] { 10, 14, 15, 15.8, 15.2, 15.9, 16, 16.1, 16.2 };
+
+            for (int i = 0; i < 9; i++)
+            {
+                chart1.Series[0].Points.AddXY(i * 200 + 700, data1[i]);
+            }
+        }
+    }
+}
